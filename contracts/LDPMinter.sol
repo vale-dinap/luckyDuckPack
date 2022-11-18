0b955397434eda3388813c3619823f187905af02
Valerio Di Napoli <valeriodinapoli@gmail.com>
2022-11-18 15:37:32 +0100

    Optimized minting payout mechanics

--- contracts/LDPMinter.sol
+++ contracts/LDPMinter.sol
@@ -8,8 +8,7 @@ import "./lib/interfaces/ILDP.sol";
 /**
  * @dev Lucky Ducks Pack Minter contract.
  */
-contract LDPMinter is Ownable, ReentrancyGuard{
-
+contract LDPMinter is Ownable, ReentrancyGuard {
     // Pricing
     uint256 private constant _price1 = 1.3 ether; // From 1 to 3333
     uint256 private constant _price2 = 1.8 ether; // From 3334 to 6666
@@ -18,37 +17,42 @@ contract LDPMinter is Ownable, ReentrancyGuard{
     uint256 public mintingStartTime;
     // Instance of the token contract
     ILDP public nft;
-    // Creator
-    address private creator;
     // LDP Rewarder contract address
     address public rewarder;
+    // Creator
+    address private creator;
+    // Total supply at last proceeds withdraw
+    uint256 private supplyAtLastWithdraw = 10; // Start at 10 due to team-reserved tokens
 
     error MaxMintsPerCallExceeded(uint256 requested, uint256 max);
-    error PaymentError();
+    error PaymentError(bool successA, bool successB);
 
     /**
      * @notice Link the token contract instance to the nft contract address.
      * Can be set only once, then it becomes immutable.
      */
-    function setNftAddress(address nftAddr) external onlyOwner{
-        require(address(nft)==address(0), "Overriding denied");
+    function setNftAddress(address nftAddr) external onlyOwner {
+        require(address(nft) == address(0), "Overriding denied");
         nft = ILDP(nftAddr);
     }
-    
+
     /**
      * @notice Set the payee address.
      */
-    function setCreatorAddress(address creatorAddr) external onlyOwner{
-        require(creatorAddr!=address(0), "Input is zero address");
+    function setCreatorAddress(address creatorAddr) external onlyOwner {
+        require(creatorAddr != address(0), "Input is zero address");
         creator = creatorAddr;
     }
 
     /**
      * @notice Set the LDP Rewarder contract address. Locked after minting starts.
      */
-    function setRewarderAddress(address rewarderAddr) external onlyOwner{
-        require(block.timestamp<mintingStartTime, "Access denied: minting started");
-        require(rewarderAddr!=address(0), "Input is zero address");
+    function setRewarderAddress(address rewarderAddr) external onlyOwner {
+        require(
+            block.timestamp < mintingStartTime,
+            "Access denied: minting started"
+        );
+        require(rewarderAddr != address(0), "Input is zero address");
         rewarder = rewarderAddr;
     }
 
@@ -59,9 +63,12 @@ contract LDPMinter is Ownable, ReentrancyGuard{
      * @param startTime Minting start time (Unix timestamp)
      */
     function initializeMinting(uint256 startTime) external onlyOwner {
-        if(mintingStartTime>0) revert("Already initialized");
-        else{
-            require(startTime>block.timestamp, "Requested time is in the past");
+        if (mintingStartTime > 0) revert("Already initialized");
+        else {
+            require(
+                startTime > block.timestamp,
+                "Requested time is in the past"
+            );
             mintingStartTime = startTime;
             _mintBatch(10);
         }
@@ -73,59 +80,76 @@ contract LDPMinter is Ownable, ReentrancyGuard{
      */
     function mint(uint256 amount) external payable nonReentrant {
         // Revert if minting hasn't started
-        require(block.timestamp>mintingStartTime, "Minting not started");
+        require(block.timestamp > mintingStartTime, "Minting not started");
         // Revert if attempting to mint more than 10 tokens at once
-        if(amount>10) revert MaxMintsPerCallExceeded({requested: amount, max:10});
+        if (amount > 10)
+            revert MaxMintsPerCallExceeded({requested: amount, max: 10});
         // Revert if underpaying
-        require(msg.value>=_currentPrice()*amount, "Price paid incorrect");
+        require(msg.value >= _currentPrice() * amount, "Price paid incorrect");
         // Finally, mint tokens
         _mintBatch(amount);
-        // Send payment to creator and LDP Rewarder contract
-        _processPayment(msg.value, amount);
     }
 
     /**
      * @notice Shows how many tokens are left to be minted.
      */
-    function mintableSupply() view external returns(uint256){
-        return nft.MAX_SUPPLY()-nft.totalSupply();
+    function mintableSupply() external view returns (uint256) {
+        return nft.MAX_SUPPLY() - nft.totalSupply();
     }
 
     /**
      * @notice Shows the current price.
      */
-    function currentPrice() view external returns(uint256){
+    function currentPrice() external view returns (uint256) {
         return _currentPrice();
+    }
+
+    /**
+     * @notice Send proceeds to creator address and incentives to Rewarder contract.
+     */
+    function withdrawProceeds() external {
+        require(mintingStartTime > 0, "Called before initializeMinting");
+        uint256 currentSupply = nft.totalSupply();
+        uint256 newSales = currentSupply - supplyAtLastWithdraw;
+        supplyAtLastWithdraw = currentSupply;
+        _processWithdraw(newSales);
     }
 
     /**
      * @dev Returns the current price (depends on the minted supply).
      */
-    function _currentPrice() view private returns(uint256){
+    function _currentPrice() private view returns (uint256) {
         uint256 curSupply = nft.totalSupply();
-        if(curSupply<3334) return _price1;
-        else if (curSupply<6667) return _price2;
+        if (curSupply < 3334) return _price1;
+        else if (curSupply < 6667) return _price2;
         else return _price3;
     }
 
     /**
      * @dev Mint multiple tokens.
      */
-    function _mintBatch(uint256 amount) private{
-        for(uint256 i=0; i<amount; ++i){
+    function _mintBatch(uint256 amount) private {
+        for (uint256 i = 0; i < amount; ++i) {
             nft.mint(msg.sender);
         }
     }
 
     /**
-     * @dev Send payment to creator address and incentives to rewarder contract.
-     * @param payAmount Total amount of ether being paid
-     * @param purchaseAmount Amount of NFTs being purchased
+     * @dev Send proceeds to creator address and incentives to rewarder contract.
+     * @param tokensSold Amount of sales
      */
-    function _processPayment(uint256 payAmount, uint256 purchaseAmount) private{
-        uint256 creatorEarnings = payAmount - (0.3 ether * purchaseAmount);
-        (bool creatorPaid, ) = creator.call{value: creatorEarnings}("");
-        (bool rewarderPaid, ) = rewarder.call{value: 0.3 ether}("");
-        if(!(creatorPaid && rewarderPaid)) revert PaymentError();
+    function _processWithdraw(uint256 tokensSold) private {
+        uint256 incentivesPerSale = 0.3 ether;
+        uint256 totalIncentives = incentivesPerSale * tokensSold;
+        if (totalIncentives < address(this).balance) {
+            uint256 creatorProceeds = address(this).balance - totalIncentives;
+            (bool creatorPaid, ) = creator.call{value: creatorProceeds}("");
+            (bool rewarderPaid, ) = rewarder.call{value: totalIncentives}("");
+            if (!(creatorPaid && rewarderPaid))
+                revert PaymentError(creatorPaid, rewarderPaid);
+        } else {
+            // Emergency measure, has no effect as long as the contract works as intended.
+            payable(creator).transfer(address(this).balance);
+        }
     }
 }
