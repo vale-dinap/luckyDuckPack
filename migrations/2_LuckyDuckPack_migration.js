040e9b3421551ec8edd800f8f2360a763e118624
Valerio Di Napoli <valeriodinapoli@gmail.com>
2023-03-18 18:04:13 +0100

    Update 2_LuckyDuckPack_migration.js

--- migrations/2_LuckyDuckPack_migration.js
+++ migrations/2_LuckyDuckPack_migration.js
@@ -2,18 +2,18 @@ const LuckyDuckPack = artifacts.require("LuckyDuckPack");
 const LDPMinter = artifacts.require("LDPMinter");
 const LDPRewarder = artifacts.require("LDPRewarder");
 
-const creatorAddr = addressGoesHere;
+const creatorAddr = "REPLACE_ME";
 
-module.exports = function (deployer) {
+module.exports = async (deployer) => {
   deployer.deploy(LuckyDuckPack);
   deployer.deploy(LDPMinter);
   deployer.deploy(LDPRewarder);
+
+  NFTcontract = await LuckyDuckPack.deployed();
+  minterContract = await LDPMinter.deployed();
+  rewarderContract = await LDPRewarder.deployed();
+
+  await minterContract.initializeContract(NFTcontract.address, rewarderContract.address, creatorAddr);
+  await rewarderContract.setNftAddress(NFTcontract.address);
+  //await NFTcontract.initialize(minterContract.address, rewarderContract.address, "base_uri_ips", "base_uri_ar"); // Need link tokens
 };
-
-NFTcontract = await LuckyDuckPack.deployed();
-minterContract = await LDPMinter.deployed();
-rewarderContract = await LDPRewarder.deployed();
-
-await NFTcontract.initialize(minterContract.address, rewarderContract.address, "base_uri_ips", "base_uri_ar");
-await minterContract.initializeContract(NFTcontract.address, rewarderContract.address, creatorAddr);
-await rewarderContract.setNftAddress(NFTcontract.address);
