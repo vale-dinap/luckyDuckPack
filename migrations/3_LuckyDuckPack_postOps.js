8597cf8dbb5ee939527d4041fec057d9ebcdb592
Valerio Di Napoli <valeriodinapoli@gmail.com>
2023-03-18 18:10:13 +0100

    Split deployments and post ops in two migration files

---
+++ migrations/3_LuckyDuckPack_postOps.js
@@ -0,0 +1,16 @@
+const LuckyDuckPack = artifacts.require("LuckyDuckPack");
+const LDPMinter = artifacts.require("LDPMinter");
+const LDPRewarder = artifacts.require("LDPRewarder");
+
+const creatorAddr = "REPLACE_ME";
+
+module.exports = async (deployer) => {
+
+  NFTcontract = await LuckyDuckPack.deployed();
+  minterContract = await LDPMinter.deployed();
+  rewarderContract = await LDPRewarder.deployed();
+
+  await minterContract.initializeContract(NFTcontract.address, rewarderContract.address, creatorAddr);
+  await rewarderContract.setNftAddress(NFTcontract.address);
+  //await NFTcontract.initialize(minterContract.address, rewarderContract.address, "base_uri_ips", "base_uri_ar"); // Need link tokens
+};
