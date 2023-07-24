c54e6f08bef0ca0aa2f1c7cfdbe6e576576cb09f
Valerio Di Napoli <valeriodinapoli@gmail.com>
2023-07-24 11:25:34 +0200

    Improved migrations

--- migrations/3_LuckyDraw_migration.js
+++ migrations/3_LuckyDraw_migration.js
@@ -1,6 +1,16 @@
-const LDPLuckyDraw = artifacts.require("LDPLuckyDraw");
+const MAINNET_LDPLuckyDraw = artifacts.require("LDPLuckyDraw");
+const TESTNET_LDPLuckyDraw = artifacts.require("LDPLuckyDraw_TESTNET");
 
 const DO_DEPLOY = process.env.DEPLOY_LUCKY_DRAW;
+const USE_TESTNET_CONTRACT = process.env.USE_LUCKYDRAW_TESTNET_CONTRACT;
+
+let LDPLuckyDraw;
+if (USE_TESTNET_CONTRACT){
+  LDPLuckyDraw = TESTNET_LDPLuckyDraw;
+}
+else {
+  LDPLuckyDraw = MAINNET_LDPLuckyDraw;
+}
 
 module.exports = function (deployer) {
   if (DO_DEPLOY) {
